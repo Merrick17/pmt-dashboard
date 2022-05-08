4e233c8d8f5d04a4690e0d3aff43e1e08b4d8694
CompendiumFi <compendiumfi@gmail.com>
2022-05-09 00:30:33 +0200

    pushing to publish

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|react-native-elements|@react-native(-community)?)/)',
-  ],
+  
 };
