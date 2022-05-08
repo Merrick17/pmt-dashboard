b99c26903d5eb1ffb10ec057caf2e421e353731f
CompendiumFi <compendiumfi@gmail.com>
2022-05-09 00:40:07 +0200

    configuring jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],"transformIgnorePatterns": [
-    "node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)"
-  ]
-  
+  presets: ['module:metro-react-native-babel-preset'],
 };
