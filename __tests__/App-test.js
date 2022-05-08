0c8a64e92e8881d7571802bda8c60e6ad3c163af
CompendiumFi <compendiumfi@gmail.com>
2022-05-09 00:54:44 +0200

    add

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,7 +7,7 @@ import React from 'react';
 import App from '../App';
 import renderer from 'react-test-renderer';
 // Note: test renderer must be required after react-native.
-const renderer = new ShallowRenderer();
+//const renderer = new ShallowRenderer();
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
