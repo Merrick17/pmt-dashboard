f922ca0f43934c142599370b7a6b166c63f2b1fb
CompendiumFi <compendiumfi@gmail.com>
2022-05-09 00:54:15 +0200

    add

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,11 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import renderer from 'react-test-renderer';
 // Note: test renderer must be required after react-native.
 const renderer = new ShallowRenderer();
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
